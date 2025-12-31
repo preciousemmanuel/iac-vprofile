910fd7162c5074691f6c5255be15faa96923a847
Emmanuel <preciousemmanuel32@gmail.com>
2025-12-31 20:40:56 +0100

    add comit

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -4,6 +4,7 @@ module "eks" {
 
   cluster_name    = local.cluster_name
   #cluster_version = "1.26"
+  #
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
